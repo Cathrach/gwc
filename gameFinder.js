bee34e9045bc10d0ab24f5c294150f6f5a07a0ae
Cathrach <qiaohu7@gmail.com>
2015-08-18 09:51:09 -0400

    testing.

--- gameFinder.js
+++ gameFinder.js
@@ -7,7 +7,7 @@ function buildList(data) {
       dataType: "jsonp",
       url: "https://itunes.apple.com/lookup?id=" + data[i].ID,
     }).done( function(data) {
-      console.log(data['results']);
+      console.log(data['results'][0]);
     })
   }
 }
