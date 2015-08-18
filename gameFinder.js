fe07b0f8d2bbc959d78e827001f287c65df3e13e
Cathrach <qiaohu7@gmail.com>
2015-08-18 09:38:08 -0400

    gah.

--- gameFinder.js
+++ gameFinder.js
@@ -3,7 +3,10 @@ var gameList;
 function buildList(data) {
   for (var i in data) {
     var iTunesData;
-    $.getJSON('https://itunes.apple.com/lookup?id=' + data[i].ID + '&callback?=', function( data ) {
+    $.ajax({
+      dataType: "jsonp",
+      url: "https://itunes.apple.com/lookup?id=" + data[i].ID,
+    }).done( function(data) {
       iTunesData = data;
     })
     console.log(data);
