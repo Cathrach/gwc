96af1de041a58bc7965b6b5251bc5296f4b8f4c3
Cathrach <qiaohu7@gmail.com>
2015-08-18 14:01:08 -0400

    fixes

--- gameFinder.js
+++ gameFinder.js
@@ -18,10 +18,10 @@ function buildList(data) {
         url: "https://itunes.apple.com/lookup?id=" + url,
       }).done( function(list) {
         // list['results'][0] is what we're looking at~
-        info = '<div class="container-fluid" style="width: 650px; height: 225px; float: right; margin-left: 10px; margin-top: 10px; border-style: solid; border-radius: 10px; border-color: teal; position: relative;"><div class="container-fluid" style="width: 150px; float: left; margin-left: 10px;"><h6>' + list['results'][0]['trackName'] + '</h6><img src="' + list['results'][0]['artworkUrl100'] + '" width="100" height="100"><br><h6>' + list['results'][0]['artistName'] + '</h6></div><div class="container-fluid" style="width: 150px; float: left;"><img src="' + list['results'][0]['screenshotUrls'][0] + '" style="height: 200px; margin-top: 10px;"></div><div class="container-fluid" style="width: 250px; margin-right: 10px; margin-left: 20px; margin-top: 20px; float: left;"><h5>Tags</h5><p>' + tagstring;
+        info = '<div class="container-fluid" style="width: 650px; height: 225px; float: right; margin-left: 10px; margin-top: 10px; border-style: solid; border-radius: 10px; border-color: teal; position: relative;"><div class="container-fluid" style="width: 150px; float: left; margin-left: 10px;"><h6>' + list['results'][0]['trackName'] + '</h6><img src="' + list['results'][0]['artworkUrl100'] + '" width="100" height="100"><br><h6>' + list['results'][0]['artistName'] + '</h6></div><div class="container-fluid" style="width: 150px; float: left;"><img src="' + list['results'][0]['screenshotUrls'][0] + '" style="height: 200px; margin-top: 10px;"></div><div class="container-fluid" style="width: 250px; margin-right: 10px; margin-left: 20px; margin-top: 20px; float: left;"><h5>Tags</h5><p>' + tagStr;
         info += '</p><a href="' + list['results'][0]['trackViewUrl'] + '">App Store</a></div><div class="container-fluid" style="width: 60px; height: 25px; margin-right: 0px; border-style: solid; border-color: blue; border-radius: 5px; position: absolute; bottom: 5px; right: 5px;"><p style="margin-left: -10px; float: left;">' + list['results'][0]['formattedPrice'] + '</p></div></div>';
 
-        gameList.push({"name": list['results'][0]['trackName'], "collapsedInfo": info, "developer": list['results'][0]['artistName'], "tags": tags});
+        gameList.push({"name": list['results'][0]['trackName'], "collapsedInfo": info, "developer": list['results'][0]['artistName'], "tags": tag});
       });
     })(tags, tagstring, data[i].ID);
   }
