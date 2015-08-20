3f7ba5bbd2b2aae9231345dcdf27c22254dca552
Cathrach <qiaohu7@gmail.com>
2015-08-20 15:41:41 -0400

    tags.

--- gameFinder.js
+++ gameFinder.js
@@ -25,7 +25,7 @@ function buildList(data) {
         info += "<h5 class='game-name'><a href='" + list['results'][0]['trackViewUrl'] + "'>" + list['results'][0]['trackName'] + "</a>: <a href='" + list['results'][0]['artistViewUrl'] + "'>" + list['results'][0]['artistName'] + "</a></h5><h5 class='game-price'>" + list['results'][0]['formattedPrice'] + "</h5></div>";
         info += "<div class='container-fluid gamePics'>";
         info += '<a href="#gameModal" class="gameIcon" data-keyboard="true" data-toggle="modal" data-img-url="' + list["results"][0]["artworkUrl100"] + '" data-name="' + list["results"][0]["trackName"] + '" data-developer="' + list["results"][0]["artistName"] + '" data-price="' + list["results"][0]["formattedPrice"] + '" data-rating="';
-        info += list["results"][0]["averageUserRatingForCurrentVersion"] + '" data-rates="' + list["results"][0]["userRatingCountForCurrentVersion"] + '" data-tags="' + tagStrLinks + '" data-ios="' + list["results"][0]["minimumOsVersion"] + '" data-description="' + list["results"][0]["description"] + '">';
+        info += list["results"][0]["averageUserRatingForCurrentVersion"] + '" data-rates="' + list["results"][0]["userRatingCountForCurrentVersion"] + '" data-tags="' + tagStr + '" data-ios="' + list["results"][0]["minimumOsVersion"] + '" data-description="' + list["results"][0]["description"] + '">';
         info += "<img src='" + list['results'][0]['artworkUrl100'] + "' class='gameIcon'/><img src='" + list['results'][0]['screenshotUrls'][0] + "' class='gameScreenshot' /></a></div>";
         info += "<div class='container-fluid gameInfo'><h4 style='float: right;'>Tags:</h4></div>";
         info += "<div class='container-fluid gameTags'><p>" + tagStr + "</p></div></div>";
